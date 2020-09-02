0b84d44e832089a03f444b776a29f3c795002219
gZYAbDhr <kucherjavyj228@gmail.com>
2020-09-02 15:12:55 +0300

    1 commit

--- src/main/java/com/epam/rd/java/basic/practice7/Main.java
+++ src/main/java/com/epam/rd/java/basic/practice7/Main.java
@@ -1,48 +1,10 @@
 package com.epam.rd.java.basic.practice7;
 
-import com.epam.rd.java.basic.practice7.entity.ObjectFactory;
-import com.epam.rd.java.basic.practice7.entity.Reserve;
-import org.w3c.dom.Document;
-
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import java.io.File;
-import java.math.BigDecimal;
-import java.io.File;
-import java.io.IOException;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-import org.xml.sax.SAXParseException;
-import org.xml.sax.helpers.DefaultHandler;
-
-public class Main
-
-{
-    public static void main(String[] args) throws JAXBException
-    {
-
-        ObjectFactory factory = new ObjectFactory();
+public class Main {
+    public static void main(String[] args) throws JAXBException {
+        SAXPars.main(new String[]{});
 
     }
 
