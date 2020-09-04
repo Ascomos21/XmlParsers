dcd7dda31bba43feac189f05b1ebc988b24c2038
gZYAbDhr <kucherjavyj228@gmail.com>
2020-09-04 21:51:53 +0300

    10 commit

--- src/main/java/com/epam/rd/java/basic/practice7/XmlToHtml.java
+++ src/main/java/com/epam/rd/java/basic/practice7/XmlToHtml.java
@@ -1,5 +1,6 @@
 package com.epam.rd.java.basic.practice7;
 
+import javax.xml.XMLConstants;
 import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
@@ -7,33 +8,36 @@ import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class XmlToHtml {
+    Logger logger = Logger.getLogger(XmlToHtml.class.getName());
+
     public static void main(String[] args) {
         XmlToHtml xmlToHtml = new XmlToHtml();
         xmlToHtml.convert();
     }
 
     public void convert() {
-        try {
-
-
+        String outputFileName = "report.html";
+        try (OutputStream htmlFile = new FileOutputStream(outputFileName);){
             TransformerFactory tFactory = TransformerFactory.newInstance();
-
+            tFactory.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, "");
+            tFactory.setAttribute(XMLConstants.ACCESS_EXTERNAL_STYLESHEET, "");
             Source xslDoc = new StreamSource("stylesheet.xsl");
 
             Source xmlDoc = new StreamSource("input.xml");
 
-            String outputFileName = "report.html";
 
-            OutputStream htmlFile = new FileOutputStream(outputFileName);
+
 
             Transformer trasform = tFactory.newTransformer(xslDoc);
 
             trasform.transform(xmlDoc, new StreamResult(htmlFile));
 
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.log(Level.WARNING, e.getMessage());
         }
     }
 }
